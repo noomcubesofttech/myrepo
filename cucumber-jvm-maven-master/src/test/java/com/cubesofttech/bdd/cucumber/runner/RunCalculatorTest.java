df8c929282504cee234fc6018bb0daac7aae8f87
cubesofttech <cubesofttech@cubesofttech-Lenovo-G40-70-001>
2016-06-01 11:24:06 +0700

    fix cannot find steps

--- cucumber-jvm-maven-master/src/test/java/com/cubesofttech/bdd/cucumber/runner/RunCalculatorTest.java
+++ cucumber-jvm-maven-master/src/test/java/com/cubesofttech/bdd/cucumber/runner/RunCalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         format = { "pretty", "html:target/cucumber", "json:target/cucumber/report.json", "junit:target/cucumber/junit.xml" },
+        glue = {"com.cubesofttech.bdd.cucumber.steps"},
         features = "classpath:cucumber/calculator.feature"
 )
 public class RunCalculatorTest {
